35a87d98d7235c69428e703d2b07796055c96455
Kenny Wilde <kendevwilde@gmail.com>
2020-07-12 10:37:12 +0100

    score increments when game is won

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,5 @@
 const gameBoard = (() => {
 
-    const gameboard = [' ',' ',' ',' ',' ',' ',' ',' ',' '];
     const gameBoardDiv = document.querySelector('.game-board');
 
     const drawCell = (cell) => {
@@ -12,80 +11,152 @@ const gameBoard = (() => {
 
     }
 
-    const drawGameBoard = (arr) => {
-        //while (gameBoardDiv.childElementCount > 0) {
-        //    gameBoardDiv.removeChild(gameBoardDiv.lastChild);
-        //}
-        arr.forEach( cell => drawCell(cell));
+    const drawGameBoard = (gameboard) => {
+
+        while (gameBoardDiv.childElementCount > 0) {
+            gameBoardDiv.removeChild(gameBoardDiv.lastChild);
+        }
+        
+        gameboard.forEach( cell => drawCell(cell));
 
     }
 
-    const updateGameBoard = (e, index, selection) => {
+    const updateGameBoard = (e, index, selection, arr) => {
 
         e.textContent = selection;
-        gameboard[index] = selection;      
+        arr[index] = selection;
+        
+    }
 
-        console.log(gameboard);
+    const endGame = (p) => {
+
+        while (gameBoardDiv.childElementCount > 0) {
+            gameBoardDiv.removeChild(gameBoardDiv.lastChild);
+        }
+
+        const endGameDiv = document.createElement('div');
+        const endGameHeader = document.createElement('h2');
+        endGameHeader.textContent = `Game Over! ${p.name} won!`;
+        endGameDiv.appendChild(endGameHeader);
+
+        const restartButton = document.createElement('button');
+        restartButton.textContent = "Restart Game";
+
+        endGameDiv.appendChild(restartButton);
+        console.log(p);
+        p.setScore(1);
+        game.updateScoreTally(p);
+        gameBoardDiv.appendChild(endGameDiv);
+
+        restartButton.addEventListener('click', game.playGame);
 
     }
 
-    return { drawGameBoard, gameboard, updateGameBoard };
+    return { drawGameBoard, updateGameBoard, endGame };
 
 })();
-
-const new_game = gameBoard.drawGameBoard(gameBoard.gameboard)
 
 const Player = (name, selection) => {
 
     let score = 0;
 
+    const getScore = () => {
+        return score;
+    }
+
     const setScore = (n) => {
-        const getScore = document.querySelector('.playerOneScore');
-        getScore.textContent = `Score: ${n}`;
+        score += n;
     }
 
-    const setName = (name) {
-        
-    }
-
-    const getName = (name) => {
-        name;
-    }
-
-    const getSelection = () => {
-        selection;
-    }
-
-    return { setScore, getName, score, selection};
+    return { setScore, name, selection, getScore};
 }
 
 
 const displayController = (() => {
 
+    let playerName = "Player 1";
+    let playerSelection = "X";
+    let playerOne = Player(playerName, playerSelection);
 
+    let playerNameTwo = "Player 2";        
+    let playerSelectionTwo = "O";    
+    let playerTwo = Player(playerNameTwo, playerSelectionTwo);
+    let currentPlayer = playerOne;
 
-    const drawSelection = () => {
+    const drawSelection = (arr) => {
+
+        currentPlayer = playerOne;
 
         const gameCells = Array.from(document.querySelectorAll('.game-board .cell'));
 
         for (i = 0; i < gameCells.length; i++) {
             gameCells[i].id = `${[i]}`;
         }
-
-
-
+        console.log(currentPlayer.selection);
         gameCells.forEach( cell => cell.addEventListener('click', (e) => {
+            
             const index = e.target.id;
-            gameBoard.updateGameBoard(e.target, index, "X");
+            
+            gameBoard.updateGameBoard(e.target, index, currentPlayer.selection, arr);
+            game.checkWin(arr, currentPlayer);
+            changeCurrentPlayer();
+            
+
         }));
     }
 
+    const changeCurrentPlayer = () => {
 
+        currentPlayer.name == playerOne.name ? currentPlayer = playerTwo : currentPlayer = playerOne;
 
+    }
 
-
-    return { drawSelection };
+    return { drawSelection, currentPlayer, changeCurrentPlayer, playerOne, playerTwo };
 
 })();
 
-const new_display = displayController.drawSelection();
+const game = (() => {
+
+    const checkWin = (arr, player) => {
+        const winningCombinations = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8]
+        ];
+
+        for (i = 0; i < winningCombinations.length; i++) {
+            const combo = winningCombinations[i];
+            
+            if(arr[combo[0]] == player.selection && arr[combo[1]] == player.selection && arr[combo[2]] == player.selection) {
+                gameBoard.endGame(player);
+            }
+        }
+    }
+
+    const updateScoreTally = (p) => {
+
+        if (p.name == "Player 1") {
+            const getScoreFieldOne = document.querySelector('#PlayerOneScore');
+            getScoreFieldOne.textContent = `Score: ${p.getScore()}`;
+        }else {
+            const getScoreFieldTwo = document.querySelector('#PlayerTwoScore');
+            getScoreFieldTwo.textContent = `Score: ${p.getScore()}`;
+        }
+    }
+
+    const playGame = () => {
+        const gameboard = [' ',' ',' ',' ',' ',' ',' ',' ',' '];
+        gameBoard.drawGameBoard(gameboard);
+        displayController.drawSelection(gameboard);
+    }
+
+    return { checkWin, playGame, updateScoreTally };
+
+})();
+
+game.playGame();
